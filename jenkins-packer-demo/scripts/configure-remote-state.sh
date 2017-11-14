a437287895b928365132c59850b221dc82a3f060
legrandmetre <31762088+legrandmetre@users.noreply.github.com>
2017-11-14 18:00:01 +0300

    commented if case

--- jenkins-packer-demo/scripts/configure-remote-state.sh
+++ jenkins-packer-demo/scripts/configure-remote-state.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # not necessary anymore in newer terraform versions, you can use a backend.tf file
-if [ "`terraform --version |head -n1`" == "Terraform v0.7.7" ] ; then
+#if [ "`terraform --version |head -n1`" == "Terraform v0.7.7" ] ; then
   terraform remote config -backend=s3 -backend-config="bucket=terraform-tfstate-0rgfh" -backend-config="key=terraform/demo-jenkins-packer" -backend-config="region=eu-central-1"
-fi
+#fi
