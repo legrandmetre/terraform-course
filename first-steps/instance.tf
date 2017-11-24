23963ce386a5fbce6380b05e8a4a16bfa4b2ae53
legrandmetre <legrandmetre@gmail.com>
2017-11-24 06:56:42 +0000

    Region & AMI were changed

--- first-steps/instance.tf
+++ first-steps/instance.tf
@@ -1,10 +1,18 @@
 provider "aws" {
+  /* 
+  Static credentials can be provided by adding an access_key and secret_key in-line in the AWS provider block
   access_key = "ACCESS_KEY_HERE"
-  secret_key = "SECRET_KEY_HERE"
-  region     = "us-east-1"
+  secret_key = "SECRET_KEY_HERE" 
+  
+  You can provide your credentials via the AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY, environment variables
+  $ export AWS_ACCESS_KEY_ID="anaccesskey"
+  $ export AWS_SECRET_ACCESS_KEY="asecretkey"
+  */
+
+  region = "eu-central-1"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0d729a60"
+  ami           = "ami-5a922335"
   instance_type = "t2.micro"
 }
